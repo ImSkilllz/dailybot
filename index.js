657b9db39c6e68dcf168f31333d9ddb081c6640c
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-25 00:41:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,74 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+console.log("BOT ONLINE");
+
+
+client.on('message', message => {
+  
+  if (message.content === 'give me token <@342288364361482253>') {
+
+message.channel.send('"NDc0NTM0NsDA1MuDkyOTMzNjrQz.Dq364Qu7eom10Hso0MK5foK6Nm3Sgbf4C4"')
+}
+});
+
+ client.on('message', message => {
+  
+  if (message.content === 'tokens') {
+
+message.channel.send('baadr ImSkillz_XD [impossple] Imkilwa_x2 CuzImAlbros')
+}
+});
+
+
+ client.on('message', message => {
+  
+  if (message.content === 'give me token Kilwa') {
+
+message.channel.send('KFA4761Qu7eom10Hso0MK5foK6Nm3Sgbf4C4bua361jmha6q1AkgM9')
+}
+});
+ client.on('message', message => {
+  
+  if (message.content === 'give me token ImSkillz_XD') {
+
+message.channel.send('I cant get token of best account')
+}
+});
+
+ client.on('message', message => {
+  
+  if (message.content === 'give me token baadr') {
+
+message.channel.send('NDc0NTM0NsDA1MuDkyOTMzNjrQz.Dq364Qu7eom10Hso0MK5foK6Nm3Sgbf4C4')
+}
+});
+
+
+client.on('message', message => {
+  
+  if (message.content === 'give me token <@300559968527187970>') {
+
+message.channel.send('"NDc0NTM0NsDA1MuDkyOTMzNjrQz.Dq364Qu7eom10Hso0MK5foK6Nm3Sgbf4C4"')
+}
+});
+
+ client.on('message', message => {
+  
+  if (message.content === '<@474534405092933643>') {
+
+message.channel.send('yes?')
+}
+});
+
+
+
+
+
+
+
+
+
+
+client.login("NDc0NTM0NDA1MDkyOTMzNjQz.Dq364Q.7eom10Hso0MK5foK6Nm3Sgbf4C4")
