38405a86ce309f86f45d3595afc292cc1aa5b847
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-26 19:47:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const token1 = "NDc0NTM0NDA1MDkyOTMzNjQz.Dq364Q.7eom10Hso0MK5foK6Nm3Sgbf4C4";
+const token2 = "NDc0MzEyNzY3OTQwMzI5NDcz.DrEXPA.tyRZG4JuhPuFvHmwMBvkI2YJoO4";
+const token3 = "NDc0Njc3MDM0NTY4NjQ2Njg0.DrJ78w.N-wm_KlZrvVY66nX8BsWd1wy3Nc";
 
 console.log("BOT ONLINE");
 
@@ -32,4 +35,6 @@ message.channel.send('Hello my Master')
 });
 
 
-
+client.login(token1);
+client.login(token2);
+client.login(token3);
