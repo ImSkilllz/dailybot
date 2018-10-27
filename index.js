b0bc1bb2e189ab97d32a3a5a542571180d2c31fd
ImSkilllz <43930333+ImSkilllz@users.noreply.github.com>
2018-10-27 11:52:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const token1 = "NDc0MzMwNjc5NjI3MzUwMDE3.DrTYnA.V2FxPiejXsWO3mwTcU0uX8GMIG0";
-const token2 = "NDc0OTQyNDI4NjcyMTYzODUx.DrTYdA.IrPz885Wibs8mGmL7wY_KBJl1po";
+const token1 = "NDc0MzA5MDY1MzcyMjA1MDc2.DrW3ew.ytfj4JcCwVvp2a0T-bmR_OIDmXA";
+const token2 = "NDc0MzA5NTM2NDcyMzY3MTE1.DrW3vg.cLTxPniMEEpGx1SClfQjdhI0K1I";
 const token3 = "NDc0Njc3MDM0NTY4NjQ2Njg0.DrJ78w.N-wm_KlZrvVY66nX8BsWd1wy3Nc";
 console.log("BOT ONLINE");
 
